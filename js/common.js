2a1bc8107990edc890cc11f12787f6f6d8d8ba23
pujx <631746321@qq.com>
2023-06-01 12:15:16 +0800

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,78 +1,105 @@
-function selCountry() {var sel = $("#country option:selected").attr("title");$("#CountryNumber").val(sel)}
-//login out
-$('.right-bar-toogle').on('click', function() {
-	$(this).toggleClass("right-bar-active").siblings().removeClass("right-bar-active");
-	if($(this).hasClass("right-bar-active")) {
-		$(this).children("a").children("i").removeClass("fa-angle-down").addClass("fa-angle-up");
-	} else {
-		$(this).children("a").children("i").removeClass("fa-angle-up").addClass("fa-angle-down");
-	};
-});
+$(function () {
+	$('.drop_box').on('click', function () {
+		var _that = this;
+		$(_that).siblings().children('.scale_up').stop().fadeOut();
+		$(_that).toggleClass("drop_active").siblings().removeClass("drop_active");
+		if ($(_that).hasClass("drop_active")) {
+			$(_that).children('.scale_up').stop().fadeIn();
+		} else {
+			$(_that).children('.scale_up').stop().fadeOut();
+		}
+	});
+	$(".scale_up,.drop_box").on("click", function (e) {
+		e.stopPropagation();
+	});
+	$(document).click(function (e) {
+		var _con = $(".drop_box");
+		if (!_con.is(e.target) && _con.has(e.target).length === 0) {
+			$(".drop_box").removeClass("drop_active");
+			$('.scale_up').stop().fadeOut();
+		}
+	});
 
-$(".scale-up,.right-bar-toogle").on("click", function(e) {
-	e.stopPropagation();
 });
-$(document).click(function(e) {
-	var _con = $(".right-bar-toogle");
-	if(!_con.is(e.target) && _con.has(e.target).length === 0) {
-		$(".right-bar-toogle").removeClass("right-bar-active");
-		$(".right-bar-toogle").children("a").children("i").removeClass("fa-angle-up").addClass("fa-angle-down");
-	}
-});
+function selCountry() {
+	var sel = $("#country option:selected").attr("title");
+	$("#CountryNumber").val(sel)
+}
 $(function() {
+	$(".qustion a").click(function(e) {
+		e.preventDefault();
+		$(this).children("i").addClass("fa-dot-circle-o");
+		$(this).children("input").prop("checked", true);
+		$(this).parent().siblings().children("a").children("i").removeClass("fa-dot-circle-o").addClass("fa-circle-o");
+		$(this).parent().siblings().children("a").children("input").removeAttr("checked");
+		$(this).addClass("experience-check").parent().siblings().children("a").removeClass("experience-check");
+		var h = $(this).attr("href");
+		$(h).addClass("answer-box-on").siblings(".answer-box-on").removeClass("answer-box-on")
+	});
+	$(".answer-chosen a").click(function() {
+		$(this).children("i").addClass("fa-dot-circle-o");
+		$(this).children("input").prop("checked", true);
+		$(this).siblings("a").children("i").removeClass("fa-dot-circle-o");
+		$(this).siblings("a").children("input").removeAttr("checked");
+	});
+	$("#trade-type").click(function() {
+		if($(this).children("i").hasClass("fa-check-square-o")) {
+			$(this).css({
+				"color": "#a7a7a7"
+			}).children("i").removeClass("fa-check-square-o").addClass("fa-square-o")
+		} else {
+			$(this).css({
+				"color": "#3A4C56"
+			}).children("i").addClass("fa-check-square-o").removeClass("fa-square-o")
+		}
+	});
+	$(".agreement-box p").click(function() {
+		if($(this).children("i").hasClass("fa-check-square-o")) {
+			$(this).children("i").removeClass("fa-check-square-o").addClass("fa-square-o");
+			$(this).children("input").removeAttr("checked");
+		} else {
+			$(this).children("i").addClass("fa-check-square-o").removeClass("fa-square-o");
+			$(this).children("input").prop("checked", true);
+		}
+	});
+	$("#checkAll").click(function() {
+		if($(this).children("i").hasClass("fa-check-square-o")) {
+			$(".agreement-clause-check div").addClass("on_check").children("span").children("i").addClass("fa-check-square-o").removeClass("fa-square-o");
+			$(".agreement-clause-check div").addClass("on_check").children("span").children("input").prop("checked", true);
+		} else {
+			$(".agreement-clause-check div").removeClass("on_check").children("span").children("i").removeClass("fa-check-square-o").addClass("fa-square-o");
+			$(".agreement-clause-check div").addClass("on_check").children("span").children("input").removeAttr("checked");
+		}
+	});
+	$(".agreement-clause-check div span").click(function() {
+		if($(this).children("i").hasClass("fa-check-square-o")) {
+			$(this).parent().removeClass("on_check");
+			$(this).children("i").removeClass("fa-check-square-o").addClass("fa-square-o");
+			$(this).children("input").removeAttr("checked");
+		} else {
+			$(this).parent().addClass("on_check");
+			$(this).children("i").addClass("fa-check-square-o").removeClass("fa-square-o");
+			$(this).children("input").prop("checked", true);
+		}
+		$(".agreement-clause-check div").each(function() {
+			if($(".agreement-clause-check div.on_check").length == 7) {
+				$("#checkAll").children("i").addClass("fa-check-square-o").removeClass("fa-square-o");
+				$("#checkAll").children("input").prop("checked", true);
+			} else {
+				$("#checkAll").children("i").removeClass("fa-check-square-o").addClass("fa-square-o");
+				$("#checkAll").children("input").removeAttr("checked");
+			}
+		})
+	});
+	$("#card-title a").click(function(e) {
+		e.preventDefault();
+		$(this).children("i").addClass("fa-dot-circle-o");
+		$(this).parent().siblings().children("a").children("i").removeClass("fa-dot-circle-o").addClass("fa-circle-o");
+		$(this).addClass("card-titles-on").parent().siblings().children("a").removeClass("card-titles-on");
+		var h = $(this).attr("href");
+		$(h).addClass("card-tabs-on").siblings(".card-tabs-on").removeClass("card-tabs-on")
+	});
 	var year = new Date();
 	var footYear = year.getFullYear();
-	$("#footer-year,.footer-year").text(footYear)
-});
-//上传证明切换
-function identity(question) {
-	if(question.value == "2") {
-		$(".select-id").css("display", "block").siblings().css('display', 'none');
-	} else if(question.value == "1") {
-		$(".select-passport").css("display", "block").siblings().css('display', "none");
-	}
-};
-
-$(function() {
-	//单选框
-	$(".radiospan").bind("click", function() {
-		$(this).addClass("on").siblings().removeClass("on");
-	});
-	//复选框
-	$(".piaochecked").click(function() {
-		if($(this).hasClass("on_check")) {
-			$(this).removeClass("on_check");
-			$(this).children().children("input").removeAttr("checked");
-		} else {
-			$(this).addClass("on_check");
-			$(this).children().children("input").prop("checked", true);
-		}
-	});
-});
-
-$(function() {
-	$("#checkAll").click(function() {
-		if($(this).hasClass("on")) {
-			$(".legal-download-box").find(".piaochecked").addClass("on_check");
-		} else {
-			$(".legal-download-box .piaochecked").removeClass("on_check");
-		}
-	});
-
-	$(".legal-download-box .piaochecked").click(function() {
-		$(".legal-download-box .piaochecked").each(function() {
-			if($(".legal-download-box .piaochecked.on_check").length == 4) {
-				$("#checkAll").addClass("on");
-				$("#uncheckAll").removeClass("on");
-			} else {
-				$("#checkAll").removeClass("on");
-
-			}
-		});
-	});
-
-	$("#uncheckAll").click(function() {
-		$(".legal-download-box .piaochecked").removeClass("on_check");
-	});
-
+	$("#footer-year").text(footYear)
 });
--- js/cropper.js
+++ js/cropper.js
@@ -1,20 +1,14 @@
-$(function() {
-	function PictureEdit() {
-		this.imageWrap = $('#J_upload_box');
-		this.uploadBtnWrap = $('#J_file_wrap');
-		this.preImg = $('#J_file_box_img');
-		this.cropImg = null;
-		//  this.uploadBtn = $('#J_file'); 
-		this.cropBtn = $('#J_crop');
-		this.pics = {};
-		this.upload();
-	}
-	PictureEdit.prototype.upload = function() {
-		var that = this;
-		that.delPics();
-		that.crop();
-	};
-	$('#J_file_wrap').on('click', function() {
+$("#cropperModal").on("hide.bs.modal", function() {
+	$(".cropper-container").remove();
+	$(".previewImg").children("img").remove()
+});
+
+function test(imgId) {
+	$("#imgId").val(imgId);
+	$("#chooseImg").attr("type", "file")
+}
+	
+$(".cropper").on('click', function() {
 		if(typeof FileReader == "undefined") {
 			$.confirm({
 				title: "Error!",
@@ -32,111 +26,30 @@ $(function() {
 			$('#cropperModal').modal('show');
 		}
 	});
-	//add
-	PictureEdit.prototype.crop = function() {
-		var that = this;
-		that.cropBtn.click(function() {
-			if($(".previewImg img").attr("src") == null) {
-				$.confirm({
-					title: "Error!",
-					content: "图片不能为空",
-					type: "orange",
-					typeAnimated: true,
-					buttons: {
-						tryAgain: {
-							text: "OK",
-							btnClass: "btn-orange",
-						},
-					}
-				});
-				return false;
-			} else {
-				that.addPics();
-			}
-		});
-	};
-	// key
-	PictureEdit.prototype.getFileKey = function() {
-		var str = '0123456789abcdefghijklmnopqrstuvwxyz',
-			n = str.length,
-			key = "",
-			i = 1;
-		while(i < n) {
-			var a = Math.floor(n * Math.random());
-			key += str.charAt(a);
-			i++;
-		}
-		return key
-	};
-	// append images
-	PictureEdit.prototype.addPics = function() {
-		var thumb = $('<div class="item"><i class="fa fa-close"></i></div>'),
-			key = this.getFileKey(),
-			data = '';
-		this.cropImg = this.preImg.cropper('getCroppedCanvas', {
-			width: 200,
-			height: 200
-		});
-		data = this.cropImg.toDataURL();
-		thumb.css('backgroundImage', 'url(' + data + ')').attr('key', key);
-		thumb.insertBefore(this.uploadBtnWrap);
-		this.pics[key] = data.split(',').pop();
-	};
-	// delete images
-	PictureEdit.prototype.delPics = function() {
-		var that = this;
-		that.imageWrap.on('click', 'i', function() {
-			var parent = $(this).parent('.item'),
-				key = parent.attr('key');
-			parent.remove();
-			delete that.pics[key];
-
-		});
-	};
-	// base 64
-	PictureEdit.prototype.getPicsData = function() {
-		var arr = [];
-		$.each(this.pics, function(i, n) {
-			arr.push(n);
-		});
-		return arr.join(',');
-	};
-	new PictureEdit();
-});
-
-$("#cropperModal").on("hide.bs.modal", function() {
-	$("#J_file").attr("type", "file");
-	$(".previewImg").children("img").remove();
-});
-$('#cropperModal').on('show.bs.modal', function(event) {
-	$(".cropper-container").hide();
-})
-
 function selectImg(file) {
 	if(!file.files || !file.files[0]) {
-		return;
+		return
 	}
-	const reader = new FileReader()
+	const reader = new FileReader();
 	const that = this;
 	reader.onloadend = function() {
-		const result = this.result
-		const img = new Image()
-		img.src = result
+		const result = this.result;
+		const img = new Image();
+		img.src = result;
 		img.onload = function() {
-			const data = compress(img, 1)
-			var imgSrc = data
-			imgArtwork = data
+			const data = compress(img, 0.5);
+			var imgSrc = data;
+			imgArtwork = data;
 			var replaceSrc = imgSrc;
-			$('#J_file_box_img').cropper('replace', replaceSrc, false);
+			$("#tailoringImg").cropper("replace", replaceSrc, false)
 		}
-	}
+	};
 	reader.readAsDataURL(file.files[0]);
-	$("#J_file").attr("type", "text");
+	$("#chooseImg").attr("type", "text")
 }
-
-$('#J_file_box_img').cropper({
+$("#tailoringImg").cropper({
 	aspectRatio: NaN,
-	preview: '.previewImg',
+	preview: ".previewImg",
 	guides: false,
 	autoCropArea: 1,
 	movable: false,
@@ -150,33 +63,56 @@ $('#J_file_box_img').cropper({
 	crop: function(e) {}
 });
 $(".cropper-rotate-btn").on("click", function() {
-	$('#J_file_box_img').cropper("rotate", 45);
+	$("#tailoringImg").cropper("rotate", 45)
 });
 $(".cropper-rotate-btnL").on("click", function() {
-	$('#J_file_box_img').cropper("rotate", -45);
+	$("#tailoringImg").cropper("rotate", -45)
 });
 var flagX = true;
 $(".cropper-scaleX-btn").on("click", function() {
 	if(flagX) {
-		$('#J_file_box_img').cropper("scaleX", -1);
-		flagX = false;
+		$("#tailoringImg").cropper("scaleX", -1);
+		flagX = false
 	} else {
-		$('#J_file_box_img').cropper("scaleX", 1);
-		flagX = true;
+		$("#tailoringImg").cropper("scaleX", 1);
+		flagX = true
 	}
-	flagX != flagX;
+	flagX != flagX
+});
+$("#sureCut").on("click", function() {
+	if($(".previewImg img").attr("src") == null) {
+		$.confirm({
+			title: "Error!",
+			content: "Choose Picture",
+			type: "danger",
+			typeAnimated: true,
+			buttons: {
+				tryAgain: {
+					text: "OK",
+					btnClass: "btn-danger",
+				}
+			}
+		});
+		return false
+	} else {
+		var cas = $("#tailoringImg").cropper("getCroppedCanvas");
+		var base64url = cas.toDataURL("image/png");
+		$("#" + $("#imgId").val()).prop("src", base64url);
+		var choose_pic = "#" + $("#imgId").val();
+		$(choose_pic).siblings("p").hide()
+	}
 });
 
 function compress(img, compressNum) {
-	const canvas = document.createElement('canvas')
-	const ctx = canvas.getContext('2d')
-	const width = img.width
-	const height = img.height
-	canvas.width = width
-	canvas.height = height
-	ctx.fillStyle = '#fff'
-	ctx.fillRect(0, 0, canvas.width, canvas.height)
-	ctx.drawImage(img, 0, 0, width, height)
-	const ndata = canvas.toDataURL('image/jpeg', compressNum)
+	const canvas = document.createElement("canvas");
+	const ctx = canvas.getContext("2d");
+	const width = img.width;
+	const height = img.height;
+	canvas.width = width;
+	canvas.height = height;
+	ctx.fillStyle = "#fff";
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+	ctx.drawImage(img, 0, 0, width, height);
+	const ndata = canvas.toDataURL("image/jpeg", compressNum);
 	return ndata
-}
+};
